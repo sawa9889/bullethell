abd6aed7a0d871b089365a87bb4b5ba79adc099b
sawa9889 <cfif.sawa@bk.ru>
2020-02-18 21:26:32 +0300

    Raw but ready to play space invaders

---
+++ collision.lua
@@ -0,0 +1,43 @@
+local collisions = {}
+local bullets   = require 'bullets'
+
+function collisions.check_rectangles_overlap( a, b )
+    local overlap = false
+    if not( a.x + a.width < b.x or b.x + b.width < a.x or
+            a.y + a.height < b.y or b.y + b.height < a.y ) then
+        overlap = true
+    end
+    return overlap
+end
+
+function collisions.invaders_bullets_collision( invaders, bullets )
+    local overlap
+    
+    for b_i, bullet in pairs( bullets.current_level_bullets) do
+        local a = { x = bullet.position_x,
+                    y = bullet.position_y,
+                    width = bullet.width,
+                    height = bullet.height }
+        
+        for i_i, invader_row in pairs( invaders.current_level_invaders ) do
+            for i_j, invader in pairs( invader_row ) do
+                local b = { x = invader.position_x,
+                            y = invader.position_y,
+                            width = invader.width,
+                            height = invader.height }
+                overlap = collisions.check_rectangles_overlap( a, b )
+                if overlap then
+                    invaders.destroy_invader( i_i, i_j )
+                    bullets.destroy_bullet( b_i )
+                end
+            end
+        end
+    end
+end
+
+
+function collisions.resolve_collisions( invaders, walls )
+    collisions.invaders_bullets_collision( invaders, bullets )
+end
+
+return collisions
--- main.lua
+++ main.lua
@@ -1,15 +1,27 @@
-local player = require 'player'
+local player 	= require 'player'
+local invaders  = require 'enemies'
+local bullets   = require 'bullets'
+local collisions= require 'collision'
 
 function love.load() -- Запускается при запуске приложения
+        invaders.construct_level()
 end
 
-function love.keyreleased( key ) -- кнопка отжата
+function love.keyreleased( key ) -- кнопка отжата`
+    if key == 'space' then
+        bullets.fire( player )
+    end
 end
 
 function love.draw() -- отрисовка каждый кадр?
-        player.draw()
+    player.draw()
+    invaders.draw()
+    bullets.draw()
 end
 
 function love.update( dt ) -- Каждый кадр
-        player.update( dt )
+    player.update( dt )
+    invaders.update( dt )
+    collisions.resolve_collisions( invaders, walls, bullets )
+    bullets.update( dt )
 end
